d54508e78c3cba716355f3372f4c692d5a01f130
Adam Hill <adam@diginc.us>
2020-01-20 23:57:18 -0600

    Giving up on monkey patching function, sed! Pipenv in docker to cache better
    Signed-off-by: Adam Hill <adam@diginc.us>

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -2,7 +2,7 @@
 # alpine sh only
 
 set -eux
-./Dockerfile.py -v --arch="${ARCH}" --hub_tag="${IMAGE}"
+#./Dockerfile.py -v --arch="${ARCH}" --hub_tag="${IMAGE}"
 # TODO: Add junitxml output and have circleci consume it
 # 2 parallel max b/c race condition with docker fixture (I think?)
 py.test -vv -n 2 -k "${ARCH}" ./test/
